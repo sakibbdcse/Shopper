9f0c55f800819f246b5a5a568e464be42126e762
sakib <sakibbd.cse@gmail.com>
2024-08-19 04:37:20 +0600

    navber design complete

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './navbar.css'
+import logo from '../../assets/logo.png'
+import cart from '../../assets/cart_icon.png'
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop")
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="shopper-logo" />
+        <p>ShopperBD</p>
+      </div>
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("shop")}}>Shop {menu=="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("men")}}>Men {menu=="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women {menu=="women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids {menu=="kids"?<hr/>:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart} alt="shopperBD cart icon" />
+        <div className="nav-cart-counter">0</div>
+      </div>
+    </div>
   )
 }
 
