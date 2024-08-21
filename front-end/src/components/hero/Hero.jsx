263b93254ee3534b6b5962fccff76a16410b2482
sakib <sakibbd.cse@gmail.com>
2024-08-21 20:03:35 +0600

    added others page

--- front-end/src/components/hero/Hero.jsx
+++ front-end/src/components/hero/Hero.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import hand_icon from '../../assets/hand_icon.png'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className="hero-left">
+        <h2>NEEW ARRIVAILS ONLY</h2>
+      </div>
+      <div className="hand-hand-icon">
+        <p>new</p>
+        <img src={hand_icon} alt="jand icon" />
+      </div>
+      <p>Collections</p>
+      <p>for evryone</p>
+      <div className="hero-latest-btn">
+        <div>latest connections</div>
+      </div>
+    </div>
   )
 }
 
