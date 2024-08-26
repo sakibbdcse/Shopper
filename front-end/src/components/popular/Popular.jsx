9e4321b93e031b277424eebd084681cd75f66254
sakib <sakibbd.cse@gmail.com>
2024-08-26 23:53:05 +0600

    added product data

--- front-end/src/components/popular/Popular.jsx
+++ front-end/src/components/popular/Popular.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './popular.css'
+import data_product from '../../assets/data'
+import Item from '../item/Item'
 const Popular = () => {
     return (
-        <div>Popular</div>
+        <div className='popular'>
+            <h1>POPULAR IN WOMEN</h1>
+            <hr />
+            <div className="popular-item">
+                {data_product.map((item, index) => {
+                    return <Item key={index} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                })}
+            </div>
+        </div>
     )
 }
 
