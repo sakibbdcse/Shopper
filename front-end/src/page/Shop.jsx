1a1c3c170449d86a983f60559fdec461f0c4658d
sakib <sakibbd.cse@gmail.com>
2024-08-27 23:57:31 +0600

    added offer section

--- front-end/src/page/Shop.jsx
+++ front-end/src/page/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/hero/Hero'
 import Popular from '../components/popular/Popular'
+import Offer from '../components/offer/Offer'
 
 const Shop = () => {
   return (
     <>
       <Hero />
       <Popular />
+      <Offer />
     </>
   )
 }
