e266b99b5050baa6bcd3ac79440f682f5f5f8ae9
sakib <sakibbd.cse@gmail.com>
2024-09-03 14:03:10 +0600

    fix product filter bugs

--- front-end/src/components/breadcrums/Breadcrums.jsx
+++ front-end/src/components/breadcrums/Breadcrums.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import './breadcrums.css'
-import breadcrum_arrow from '../../assets/breadcrum_arrow.png'
-const Breadcrums = (props) => {
-    const { product } = props
-    return (
-        <div className='breadcrums'>
-            Home <img src={breadcrum_arrow} alt="breadcrum arrow icon" /> Shop <img src={breadcrum_arrow} alt="breadcrum arrow icon" /> {product.category} <img src={breadcrum_arrow} alt="breadcrum arrow icon" /> Shop <img src={breadcrum_arrow} alt="breadcrum arrow icon" /> {product.name}
-        </div>
-    )
-}
+import React from 'react';
+import './breadcrums.css';
+import breadcrum_arrow from '../../assets/breadcrum_arrow.png';
 
-export default Breadcrums
+const Breadcrums = ({ product }) => {
+    if (!product) {
+        return (
+            <div>
+                <h1>Product not found</h1>
+            </div>
+        );
+    }
+
+    const breadcrumbs = [
+        { label: 'Home', link: '/' },
+        { label: 'Shop', link: '/shop' },
+        { label: product.category, link: `/shop/${product.category}` },
+        { label: product.name, link: null }
+    ];
+
+    return (
+        <nav className='breadcrums'>
+            {breadcrumbs.map((crumb, index) => (
+                <span key={index}>
+                    {crumb.link ? (
+                        <a href={crumb.link}>{crumb.label}</a>
+                    ) : (
+                        <span>{crumb.label}</span>
+                    )}
+                    {index < breadcrumbs.length - 1 && (
+                        <img src={breadcrum_arrow} alt="breadcrumb arrow icon" />
+                    )}
+                </span>
+            ))}
+        </nav>
+    );
+};
+
+export default Breadcrums;
--- front-end/src/page/Product.jsx
+++ front-end/src/page/Product.jsx
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react';
 import { ShopContext } from './../context/ShopContext';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import Breadcrums from '../components/breadcrums/Breadcrums';
+import ProductDisplay from '../components/productDisplay/ProductDisplay';
+
 const Product = () => {
-  const { all_product } = useContext(ShopContext)
-  const { productId } = useParams()
-  const product = all_product.find((e) => e.id === Number(productId))
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const foundProduct = all_product.find((e) => e.id === Number(productId));
+    setProduct(foundProduct);
+    setLoading(false);
+  }, [all_product, productId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!product) {
+    return <p>Product not found</p>;
+  }
+
   return (
     <div>
       <Breadcrums product={product} />
+      <ProductDisplay product={product} />
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
